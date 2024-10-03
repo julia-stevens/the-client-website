6aed634761dd701bbbd2e42d0bd9857c889e0829
julia-stevens <juliastevens02@gmail.com>
2024-10-03 12:44:34 +0200

    #12 #13 Header met hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
-// JS
+const primaryMenu = document.getElementById("primary-menu");
+const checkBox = document.getElementById("hamburger-menu");
+
+checkBox.addEventListener("click", function() {
+    primaryMenu.classList.toggle("clicked")
+  });
